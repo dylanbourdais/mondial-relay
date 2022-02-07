50bf8601073fd64126ce330274efa05f1c27d876
Kévin Robic <kevin.r@institutsolacroup.com>
2022-02-07 13:59:50 +0100

    schema joi address

--- back/utilities/schemaValidateAddress.js
+++ back/utilities/schemaValidateAddress.js
@@ -2,15 +2,10 @@ const Joi = require("joi");
 
 const validate = (obj) => {
   const schema = Joi.object({
-    street: Joi.string()
-      .regex(/^[0-9A-Z_\-'., /]{2,32}$/)
-      .required(),
-    compl: Joi.string()
-      .regex(/^[0-9A-Z_\-'., /]{2,32}$/)
-      .required(),
+    street: Joi.string().required(),
+    compl: Joi.string().required(),
     city: Joi.string()
-      .regex(/^[A-Z_\-' ]{2,26}$/)
-      .required(),
+    .required(),
     zip: Joi.string()
       .regex(/^[0-9@]{1}[0-9]{4}$/)
       .required(),
